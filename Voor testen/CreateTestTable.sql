dd64e0ab79b344d6c96212f239f0016767fe763d
Pytrik Elzinga <pytrik.elzinga@gmail.com>
2015-06-27 14:16:02 +0200

    Initial commit, quity messy also doesn't really work

---
+++ Voor testen/CreateTestTable.sql
@@ -0,0 +1,15 @@
+IF OBJECT_ID('Books') IS NOT NULL DROP TABLE Books;
+CREATE TABLE Books (BookId BIGINT IDENTITY(1,1) PRIMARY KEY, BookCode NVARCHAR(100), Author NVARCHAR(255), Title NVARCHAR(255), Genre NVARCHAR(255), PublishDate DATE, Price MONEY, Description TEXT, NrOfPages BIGINT)	
+
+TRUNCATE TABLE Books
+INSERT INTO Books (BookCode, Author, Title) VALUES ('bk999', 'SomeDude', 'Dude!')
+INSERT INTO Books (BookCode, Author, Title) VALUES ('bk998', 'SomeOtherDude', 'WUT?')
+
+MERGE Books b
+USING Books x
+ON b.BookCode = x.BookCode + 'x'
+WHEN NOT MATCHED THEN
+	INSERT (BookCode, Author, Title, Genre, PublishDate, Price, Description)
+	VALUES (x.BookCode+'x', x.Author, x.Title, x.Genre, x.PublishDate, x.Price, x.Description);
+
+SELECT * FROM Books	
---
+++ Voor testen/ImportXML.ps1
@@ -0,0 +1,40 @@
+﻿param (
+    [System.Data.IDbConnection] $DatabaseConnection
+)
+
+Push-Location $PSScriptRoot;
+
+try {
+    Import-Module "E:\PSDapper\bin\Debug\PSDapper.dll";
+
+    if(!$DatabaseConnection) {
+        $DatabaseConnection = New-Object System.Data.SqlClient.SqlConnection;
+        $DatabaseConnection.ConnectionString = "Data Source=localhost;Initial Catalog=ImportXMLTest;Integrated Security=SSPI;";
+    }
+    
+    $Params = New-Object System.Object;
+    $Params | Add-Member -MemberType NoteProperty -name Pages -value 276;
+
+    #$Params =  New-Object System.Object({ Pages = 378 });
+    $Params.GetType();
+
+    $XML = [XML] "<Pages>873</Pages>";
+
+    $XML.GetType();
+
+    Dapper-Execute -DbConnection $DatabaseConnection -SQLCommand "INSERT INTO Books (BookCode, Author, Title, NrOfPages) VALUES ('bk997', 'Meeeeee!', 'LALALALAL', @Pages)" -SQLParameters $XML #$Params #@{ "Pages" = 181 }
+
+    #Dapper-Execute -DbConnection $DatabaseConnection -SQLCommand
+
+    #$XML = [XML] (Get-Content $XMLFilePath);
+    #$XML.catalog.book | MergeInto-DBTable -Database $DBConnection -Table $TableName;
+}
+catch {
+    throw;
+}
+finally {
+    $DatabaseConnection.Dispose();
+    Pop-Location;
+}
+
+#Export-ModuleMember -function New-Function0
